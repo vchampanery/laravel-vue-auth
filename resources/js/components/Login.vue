912b76ca74d3ea58d3c6428392659ffeb65a21c3
viral champanery <vchampanery@gmail.com>
2024-10-19 15:12:52 +0530

    initial commit

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <form @submit.prevent="login">
+        <input v-model="email" type="email" placeholder="Email" required />
+        <input v-model="password" type="password" placeholder="Password" required />
+        <button type="submit">Login</button>
+        <div v-if="error">{{ error }}</div>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            error: null,
+        };
+    },
+    methods: {
+        async login() {
+            try {
+                // Call the login API
+            } catch (err) {
+                this.error = 'Login failed';
+            }
+        },
+    },
+};
+</script>
---
+++ resources/js/components/Register.vue
@@ -0,0 +1,31 @@
+<template>
+    <form @submit.prevent="register">
+        <input v-model="name" type="text" placeholder="Name" required />
+        <input v-model="email" type="email" placeholder="Email" required />
+        <input v-model="password" type="password" placeholder="Password" required />
+        <button type="submit">Register</button>
+        <div v-if="error">{{ error }}</div>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            email: '',
+            password: '',
+            error: null,
+        };
+    },
+    methods: {
+        async register() {
+            try {
+                // Call the registration API
+            } catch (err) {
+                this.error = 'Registration failed';
+            }
+        },
+    },
+};
+</script>
